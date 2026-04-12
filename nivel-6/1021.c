679530618c7b885355b676aab926c3f443686445
PuFh <137668343+PuFh@users.noreply.github.com>
2026-04-12 16:14:42 -0300

    Update 1021.c

--- nivel-6/1021.c
+++ nivel-6/1021.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int inteiro;
 
-    inteiro = 10;
-    return 0;
+
+int main(){
+    float valor;
+    int i;
+    scanf("%.2f",&valor);
+    
+    if(valor > 100.00){
+        for(i = 0; i < )
+    }
 }
