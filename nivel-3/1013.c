fd572c1d6894de93cb6a04b706fa5afb5a6e4818
PuFh <137668343+PuFh@users.noreply.github.com>
2026-04-13 13:09:04 -0300

    training

---
+++ nivel-3/1013.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a,b,c, maior;
+
+    scanf("%d",&a);
+    scanf("%d",&b);
+    maior = (a+b+abs(a-b))/2;
+    scanf("%d",&c);
+
+    maior = (maior+c+abs(maior-c))/2;
+
+    printf("%d eh o maior\n",maior);
+
+
+    return 0;
+}
